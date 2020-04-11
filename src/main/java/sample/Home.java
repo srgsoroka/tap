dd4dff9c283a66531056f45e3469b78250895838
SSOROKA <Sergiy.Soroka@refinitiv.com>
2020-04-11 18:01:31 +0300

    Added DatePicker

--- src/main/java/sample/Home.java
+++ src/main/java/sample/Home.java
@@ -1,9 +1,12 @@
 package sample;
 
+import java.util.Date;
 import org.apache.tapestry.IPage;
 import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.annotations.InitialValue;
 import org.apache.tapestry.annotations.InjectPage;
+import org.apache.tapestry.form.IPropertySelectionModel;
+import org.apache.tapestry.form.StringPropertySelectionModel;
 import org.apache.tapestry.html.BasePage;
 
 public abstract class Home extends BasePage {
@@ -16,13 +19,20 @@ public abstract class Home extends BasePage {
 
     public abstract void setStockId(String stockId);
 
+    public abstract Date getQuoteDate();
+
+    public IPropertySelectionModel getAvailStockIds() {
+        return new StringPropertySelectionModel(new String[]{"IDM", "MSFT", "RHAT"});
+    }
+
     public IPage onOk(IRequestCycle cycle) {
         //System.out.println("Listener called. Stock id is: " + stockId);
         //cycle.activate("Result");
-        int stockValue = getStockId().hashCode() % 10;
+        int stockValue = (getStockId() + getQuoteDate().toString()).hashCode() % 10;
+
         /*Result resultPage = (Result) cycle.getPage("Result");
         resultPage.setStockValue(stockValue);*/
-        Result resultPage=getResultPage();
+        Result resultPage = getResultPage();
         resultPage.setStockValue(stockValue);
         return resultPage;
     }
